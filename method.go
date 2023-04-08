f0beed579f8e936f01080ad77bd30bb728bed016
Joseph <ivan_whisper@outlook.com>
2023-04-08 22:07:32 +0800

    with aop

--- method.go
+++ method.go
@@ -136,3 +136,45 @@ func GracefulRun(ctx context.Context, e http.Handler, addr string, timeout time.
 		break
 	}
 }
+
+func GracefulRunWithAop(ctx context.Context, e http.Handler, addr string, timeout time.Duration, before func(port int), after func()) {
+	defer after()
+	// bind ip&port
+	srv := &http.Server{
+		Handler: e,
+	}
+	errCh := make(chan error, 1)
+	defer close(errCh)
+	// listen
+	go func() {
+		ln, err := net.Listen("tcp", addr)
+		if err != nil {
+			errCh <- err
+			return
+		}
+		port := ln.Addr().(*net.TCPAddr).Port
+		before(port)
+		// service connections
+		if err := srv.Serve(ln); err != nil {
+			errCh <- err
+		}
+	}()
+
+	notifyCtx, stop := signal.NotifyContext(ctx, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	select {
+	case s := <-notifyCtx.Done():
+		stop()
+		logger.Info(fmt.Sprintf("Shutdown: Receive Sign(%s)", s))
+		timeoutCtx, cancel := context.WithTimeout(ctx, timeout)
+		defer cancel()
+		if err := srv.Shutdown(timeoutCtx); err != nil {
+			logger.Error(fmt.Sprintf("Shutdown: %s", err))
+		}
+		logger.Info("Shutdown: exit")
+		break
+	case err := <-errCh:
+		logger.Error(fmt.Sprintf("Listen: Receive Error %s", err))
+		break
+	}
+}
